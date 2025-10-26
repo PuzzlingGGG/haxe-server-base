112c45255ad175939be4c0b6cf675e716fecfb43
PuzzlingGGG <138084889+PuzzlingGGG@users.noreply.github.com>
2025-10-26 16:38:26 -0400

    oops

--- Api.hx
+++ Api.hx
@@ -3,9 +3,9 @@ class Api {
 
     @:get('/status')
     @:produces('application/json')
-    @:header("Access-Control-Allow-Origin: *")
-    @:header("Access-Control-Allow-Methods: GET, POST, OPTIONS")
-    @:header("Access-Control-Allow-Headers: Content-Type")
+    @:header("Access-Control-Allow-Origin" "*")
+    @:header("Access-Control-Allow-Methods" "GET, POST, OPTIONS")
+    @:header("Access-Control-Allow-Headers" "Content-Type")
     public function stats():String {
         return ("ok");
     }
